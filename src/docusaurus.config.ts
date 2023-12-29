019eb536b99ee811ebce04ed22cc85392382b523
Inqnuam <vvanderlian@gmail.com>
2023-12-29 12:04:35 +0100

    fix: site head description
    fix: broken links in code blocks

--- src/docusaurus.config.ts
+++ src/docusaurus.config.ts
@@ -2,6 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
+export const baseUrl = "/dynamoql-site/";
+
 const config: Config = {
   title: "DynamoQL",
   tagline: "Transforming DynamoDB Complexity into Developer Simplicity.",
@@ -11,7 +13,7 @@ const config: Config = {
   url: "https://inqnuam.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/dynamoql-site/",
+  baseUrl,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -28,7 +30,7 @@ const config: Config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-  clientModules: ["./src/scripts/typeLink.ts"],
+  // clientModules: ["./src/scripts/typeLink.ts"],
   presets: [
     [
       "classic",
--- src/src/scripts/typeLink.ts
+++ src/src/scripts/typeLink.ts
@@ -1,9 +1,11 @@
+import { baseUrl } from "@site/docusaurus.config";
+
 export const textToTypeLink = {
-  " Boolean": "/docs/schema/data-types/Boolean",
-  " String": "/docs/schema/data-types/String",
-  " Number": "/docs/schema/data-types/Number",
-  " Array": "/docs/schema/data-types/Array",
-  " Set": "/docs/schema/data-types/Set",
+  " Boolean": `${baseUrl}docs/schema/data-types/Boolean`,
+  " String": `${baseUrl}docs/schema/data-types/String`,
+  " Number": `${baseUrl}docs/schema/data-types/Number`,
+  " Array": `${baseUrl}docs/schema/data-types/Array`,
+  " Set": `${baseUrl}docs/schema/data-types/Set`,
 };
 
 export const clickHandler = (e) => {
